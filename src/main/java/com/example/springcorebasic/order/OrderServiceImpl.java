1bbb47f7f6d09340d22a2636181d97a9e8886559
jaeho0613 <ywm2004@gmail.com>
2024-06-02 22:32:06 +0900

    [스프링 핵심 원리 - 기본편 # 섹션 2] - 스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    새로운 할인 정책 적용과 문제점

--- src/main/java/com/example/springcorebasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/springcorebasic/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.springcorebasic.order;
 
 import com.example.springcorebasic.discount.DiscountPolicy;
-import com.example.springcorebasic.discount.FixDiscountPolicy;
 import com.example.springcorebasic.member.Member;
 import com.example.springcorebasic.member.MemberRepository;
 import com.example.springcorebasic.member.MemoryMemberRepository;
@@ -9,13 +8,13 @@ import com.example.springcorebasic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    
+    private DiscountPolicy discountPolicy;
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        
+
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
