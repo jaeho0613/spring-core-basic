0182cd656c10b24dade23e8ad18d3a4bfb6fb0f0
jaeho0613 <ywm2004@gmail.com>
2024-06-07 22:30:55 +0900

    [스프링 핵심 원리 - 기본편 # 섹션 4] 스프링 빈 조회 - 기본

---
+++ src/test/java/com/example/springcorebasic/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,40 @@
+package com.example.springcorebasic.beanfind;
+
+import com.example.springcorebasic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    
+    @Test
+    @DisplayName("모든빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("모든빈 출력하기")
+    void findApplicationAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // BeanDefinition.ROLE_APPLICATION      : 일반적으로 사용자가 정의한 빈
+            // BeanDefinition.ROLE_INFRASTRUCTURE   : 스프링이 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+
+
+}
